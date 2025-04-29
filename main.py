d13540ece0a6726a71702660aa2c009417dc7267
nattntn <natthanich.h@kkumail.com>
2025-04-29 20:14:37 +0700

    index

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ from tensorflow.keras.preprocessing.image import img_to_array
 
 
 # โหลดโมเดล ONNX
-onnx_model_path = "./model.onnx"
-session = ort.InferenceSession(onnx_model_path)
+model_7_23 = ort.InferenceSession("./Model_app/Duo_7_23.onnx")
+model_7_14 = ort.InferenceSession("./Model_app/Duo_7_14.onnx")
+model_15_23 = ort.InferenceSession("./Model_app/Duo_15_23.onnx")
 
 #-----------------------------------------------------------------------------------------------------------------
 # cut_image.py
@@ -21,19 +22,25 @@ def cut_image(image):
     cropped_right = image.crop((crop_right_width, 0, width, height)).transpose(Image.FLIP_LEFT_RIGHT)
     return cropped_left, cropped_right
 #---------------------------------------------------------------------------------------------------------------------
-
+# ฟังก์ชันการเลือกตัวแบบ
+def select_model(age_pred):
+    if 7 <= age_pred <= 14:
+        return model_7_14
+    else: 
+        return model_15_23
+#---------------------------------------------------------------------------------------------------------------------
 # ฟังก์ชันการทำนาย
-def predict_image(img, session, height, width):
+def predict_image(img, model, height, width):
     img = img.resize((height, width))
     x = img_to_array(img) / 255.0
     x = np.expand_dims(x, axis=0)
 
-    input_name = session.get_inputs()[0].name
-    output_name_regression = session.get_outputs()[0].name
-    output_name_classification = session.get_outputs()[1].name
+    input_name = model.get_inputs()[0].name
+    output_name_regression = model.get_outputs()[0].name
+    output_name_classification = model.get_outputs()[1].name
 
-    regression_result = session.run([output_name_regression], {input_name: x})[0]
-    classification_result = session.run([output_name_classification], {input_name: x})[0]
+    regression_result = model.run([output_name_regression], {input_name: x})[0]
+    classification_result = model.run([output_name_classification], {input_name: x})[0]
 
     return regression_result, classification_result
 #-----------------------------------------------------------------------------------------------------------------------------------
@@ -53,29 +60,61 @@ def calculate_confident(value):
 #-----------------------------------------------------------------------------------------------------------------
 # ฟังก์ชันหาค่าอายุและเพศ
 def predict_age_gender(img_paths):
+    # ใช้ model 7-23 คัดช่วงอายุ
     pred_list_regression = []
     pred_list_classification = []
 
     for img in img_paths:
-        height = width = session.get_inputs()[0].shape[2]
-        regression_result, classification_result = predict_image(img, session, height, width)
+        height = width = model_7_23.get_inputs()[0].shape[2]
+        regression_result, classification_result = predict_image(img, model_7_23, height, width)
         pred_list_regression.append(regression_result)
         pred_list_classification.append(classification_result)
 
+    # มี 2 รูป ซ้าย ขวา    
     con_0 = calculate_confident(pred_list_classification[0][0])
     con_1 = calculate_confident(pred_list_classification[1][0])
 
     if con_0 > con_1:
-        gender_pred = "Male" if pred_list_classification[0][0] >= 0.5 else "Female"
-        age_pred = np.around(pred_list_regression[0][0])
+        selected_img = img_paths[0]
+        age_estimated = np.around(pred_list_regression[0][0])
     else:
-        gender_pred = "Male" if pred_list_classification[1][0] >= 0.5 else "Female"
-        age_pred = np.around(pred_list_regression[1][0])
+        selected_img = img_paths[1]
+        age_estimated = np.around(pred_list_regression[1][0])
 
-    confidence = max(con_0, con_1)
+    model_selected = select_model(age_estimated)
+
+    # ใช้โมเดลเฉพาะกลุ่มทำนายใหม่ (อายุ+เพศ)
+    height = width = model_selected.get_inputs()[0].shape[2]
+    regression_result, classification_result = predict_image(selected_img, model_selected, height, width)
+
+    age_pred   = np.around(regression_result[0])
+    gender_pred = "Male" if classification_result[0] >= 0.5 else "Female"
+    confidence = calculate_confident(classification_result[0])
+
     return age_pred, gender_pred, confidence
 
 #---------------------------------------------------------------------------------
+# recall
+recall_by_age = {
+    7: 0.76,
+    8: 0.67,
+    9: 0.68,
+    10: 0.49,
+    11: 0.30,
+    12: 0.54,
+    13: 0.36,
+    14: 0.17,
+    15: 0.37,
+    16: 0.66,
+    17: 0.29,
+    18: 0.30,
+    19: 0.27,
+    20: 0.14,
+    21: 0.16,
+    22: 0.02,
+    23: 0.00
+}
+#------------------------------------------------------------------------------------
 # เก็บรูป
 import os
 if not os.path.exists("uploads"):
@@ -104,10 +143,13 @@ if uploaded_file is not None:
     
     # เรียกฟังก์ชันทำนายอายุและเพศจากภาพทั้งสอง
     age, gender, confidence = predict_age_gender([left_img, right_img])
+    recall_value = recall_by_age[int(age)]
 
     # แสดงผลการทำนาย
     st.subheader("Prediction Results")
-    st.write(f"<span style='font-size:24px;'> <b>Age</b>: <span style='color:green;'><b>{int(age)}</b></span></span><span style='font-size:20px;'> years</span>", unsafe_allow_html=True)
+    st.write(f"<span style='font-size:24px;'> <b>Age</b>: <span style='color:green;'><b>{int(age)}</b></span></span>"
+             f"<span style='font-size:20px;'> years  <span style='margin-left: 10px;'>(Recall: {recall_value * 100:.2f}% )</span></span>",
+             unsafe_allow_html=True)
     st.write(f"<span style='font-size:24px;'> <b>Sex</b>: <span style='color:green;'><b>{gender}</b></span> </span>", unsafe_allow_html=True)
     st.write(f"<span style='font-size:22px;'> <b>Confidence</b>: <span style='color:blue;'>{confidence.item()*100:.2f}%</span> </span>", unsafe_allow_html=True)
 
